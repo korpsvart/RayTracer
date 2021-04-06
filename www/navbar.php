4f64c681e879320ddeb5554422d035663287136e
korpsvart <riccardodibella9@gmail.com>
2021-04-06 20:44:53 +0200

    added files for website, added uikit files

---
+++ www/navbar.php
@@ -0,0 +1,13 @@
+<div class="uk-container uk-container-small">
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+                <li class="uk-active"><a href=""></a></li>
+                <li class="uk-parent"><a href=""></a></li>
+                <li class="uk-position-left"><a href="index.html">Presentazione</a></li>
+                <li class="uk-position-center"><a href="theory.html">Teoria</a></li>
+                <li class="uk-position-right"><a href="download.html">Download</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
